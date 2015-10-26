0d42374358ccae08230a334a0aa1e45fc03bf809
Joseph Coffland <joseph@cauldrondevelopment.com>
2015-10-26 13:23:21 -0700

    Updated release script

--- master/scripts/publish_release.sh
+++ master/scripts/publish_release.sh
@@ -3,11 +3,10 @@
 SRC=/home/buildmaster/releases
 DST=root@coffland.com:/var/www/camotics.org/http/releases
 
-eval $(ssh-agent)
+read -sp "Password: " PASS
 
 for MODE in release debug; do
-    rsync -av --max-delete=-1 --progress $SRC/public/$MODE/camotics \
-        $DST/public/$MODE/camotics
+    sshpass -p "$PASS" \
+        rsync -av --max-delete=-1 --progress $SRC/public/$MODE/camotics/ \
+        $DST/public/$MODE/camotics/
 done
-
-kill $SSH_AGENT_PID
