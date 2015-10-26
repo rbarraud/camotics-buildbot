a15acebc2d7711d07d9b73f89527f01c194a435f
Joseph Coffland <joseph@cauldrondevelopment.com>
2015-10-26 12:44:37 -0700

    Updated release script

--- master/scripts/publish_release.sh
+++ master/scripts/publish_release.sh
@@ -3,7 +3,11 @@
 SRC=/home/buildmaster/releases
 DST=root@coffland.com:/var/www/camotics.org/http/releases
 
+eval $(ssh-agent)
+
 for MODE in release debug; do
     rsync -av --max-delete=-1 --progress $SRC/public/$MODE/camotics \
         $DST/public/$MODE/camotics
 done
+
+kill $SSH_AGENT_PID
